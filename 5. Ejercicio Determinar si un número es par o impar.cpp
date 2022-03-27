f26916f604e39a8dbc727cd005ff268570b02afc
Henry <hrodriguezhenry@gmail.com>
2022-03-27 16:39:41 -0600

    Eso

---
+++ 5. Ejercicio Determinar si un número es par o impar.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void pdhrodriguezhenry(){
+	cout <<"Me llamo henry personal";
+}
+
+
+int main(){
+	int numero;
+	
+	cout<<"Digite un numero: ";
+	cin>>numero;
+	
+	if(numero==0){
+		cout<<"El numero es cero";
+	}
+	else if(numero%2==0){	// "%" Divide a la variable "numero"y le saca el residuo, se valida que el residuo sea igual a "0"
+		cout<<"El numero es par";
+	}
+	else{
+		cout<<"El numero es impar";
+	}
+	
+	return 0;
+}
